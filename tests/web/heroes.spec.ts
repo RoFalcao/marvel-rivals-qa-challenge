0082c6080c9df53d5f5bf624298e6db19428db44
RoFalcao <rosesfalcao@gmail.com>
2026-08-25 20:42:51 -0300

    Adiciona testes de carregamento e listagem de heróis

--- tests/web/heroes.spec.ts
+++ tests/web/heroes.spec.ts
@@ -1,10 +1,25 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Marvel Rivals Heroes - Web', () => {
+
     test('UI-01 - deve carregar a página de heróis com sucesso', async ({ page }) => {
         await page.goto('/heroes/index.html');
 
         await expect(page).toHaveURL(/heroes/);
         await expect(page).toHaveTitle(/Marvel Rivals/i);
     });
+
+    test('UI-02 - deve exibir a lista de heróis', async ({ page }) => {
+        await page.goto('/heroes/index.html');
+
+        const moreHeroesButton = page.locator('.more-btn');
+
+        await expect(moreHeroesButton).toBeVisible();
+        await moreHeroesButton.click({ force: true });
+
+        const heroesList = page.getByRole('list').nth(4);
+
+        await expect(heroesList).toBeVisible();
+    });
+
 });
