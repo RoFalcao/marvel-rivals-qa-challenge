1c402ee1ad8ae23c87d8d07d2346a2b76407e7c0
RoFalcao <rosesfalcao@gmail.com>
2026-08-25 21:25:39 -0300

    Adiciona cenários automatizados da jornada de heróis

--- tests/web/heroes.spec.ts
+++ tests/web/heroes.spec.ts
@@ -22,4 +22,53 @@ test.describe('Marvel Rivals Heroes - Web', () => {
         await expect(heroesList).toBeVisible();
     });
 
+    test('UI-03 - deve navegar para os detalhes de um herói', async ({ page }) => {
+        await page.goto('/heroes/index.html');
+
+        const moreHeroesButton = page.locator('.more-btn');
+
+        await expect(moreHeroesButton).toBeVisible();
+        await moreHeroesButton.click({ force: true });
+
+        const heroesList = page.getByRole('list').nth(4);
+        await expect(heroesList).toBeVisible();
+
+        const heroCards = heroesList.locator('li');
+        const selectedHero = heroCards.nth(30);
+
+        await selectedHero.click();
+
+        await expect(page).toHaveURL(/\/heroes\/index\.html\?id=/);
+
+        const abilitiesSection = page
+            .getByRole('heading', { name: 'ABILITIES', exact: true })
+            .first();
+
+        await abilitiesSection.scrollIntoViewIfNeeded();
+        await expect(abilitiesSection).toBeVisible();
+    });
+
+    test('UI-05 - deve renderizar a imagem do herói', async ({ page }) => {
+        await page.goto('/heroes/index.html');
+
+        const moreHeroesButton = page.locator('.more-btn');
+
+        await expect(moreHeroesButton).toBeVisible();
+        await moreHeroesButton.click({ force: true });
+
+        const heroesList = page.getByRole('list').nth(4);
+        await expect(heroesList).toBeVisible();
+
+        const heroCards = heroesList.locator('li');
+        const selectedHero = heroCards.nth(6);
+
+        await selectedHero.click();
+
+        await expect(page).toHaveURL(/\/heroes\/index\.html\?id=/);
+
+        const heroImage = page.locator('.jyImg');
+
+        await expect(heroImage).toBeVisible();
+    });
+
 });
